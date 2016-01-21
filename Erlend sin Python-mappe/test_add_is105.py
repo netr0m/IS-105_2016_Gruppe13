9935018711cd858d512266dc04b429ec954fe8c0
Erlend <erlendsaetre@gmail.com>
2016-01-21 16:27:52 +0100

    endringer

---
+++ Erlend sin Python-mappe/test_add_is105.py
@@ -0,0 +1,13 @@
+import unittest
+
+from is105 import add
+
+class TestAdd(unittest.TestCase):
+    def setUp(self):
+        pass
+    def test_numbers_5_7(self):
+        self.assertEqual(add(5,7), 11)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
