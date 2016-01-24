035b6273175d7009e060db192c9792f6cde61b5a
mortea15 <mortea15@student.uia.no>
2016-01-24 22:47:41 +0100

    Fix

--- pluss.py
+++ pluss.py
@@ -1,5 +1,7 @@
 #Nora
-x = raw_input ("Velg et nummer for aa summere ")
-y = raw_input ("Velg et nummer for aa summere ")
+x = input("Velg et nummer for aa summere ")
+y = input("Velg et nummer for aa summere ")
 
-print x + y
+sum = x + y
+
+print "Summen av %r og %r er" % (x,y), sum
