5225c699d057d730bce1df8910e012da898d7e1b
Erlend <erlendsaetre@gmail.com>
2016-01-25 12:31:28 +0100

    bare en testfil

---
+++ erlend-oppgave.py
@@ -0,0 +1,7 @@
+# Morten
+# This is a function which returns true/false if x is greater than y
+x = raw_input("Type a number to check if it is greater than y ")
+y = raw_input("Type a number to check towards x %r " % (x))
+
+print "Is %r greater than %r ? " % (x, y)
+print x > y
