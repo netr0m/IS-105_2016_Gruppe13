e0bccb842188e3ee508e58649ea0ccb814f570b5
mortea15 <mortea15@student.uia.no>
2016-01-25 16:22:36 +0100

    Fjernet filer fra master, flyttet over til develop

--- minus.py
+++
@@ -1,8 +0,0 @@
-# Erlend
-# This function substract two number and print an output
-a = int(raw_input("Press your first number "))
-b = int(raw_input("Then your second number "))
-
-sum = a - b
-
-print sum
