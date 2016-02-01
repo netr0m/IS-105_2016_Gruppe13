8e2a54a54ef2a4efc0a984720a3608f08a20f512
mortea15 <mortea15@student.uia.no>
2016-02-01 22:30:03 +0100

    Lastet opp uke04 ascii2binary dekoder + ascii2binary translator

--- uke4/translator + decoder/ascii2binary.py
+++ uke4/translator + decoder/ascii2binary.py
@@ -1,29 +1,43 @@
-# -*- coding: utf-8 -*-
+## -*- coding: utf-8 -*-
 '''
 Module med eksempler i uke 04 (informasjonsteori)
 Løsninger for klasseoppgavene 25.01.2016 implementeres her
 Løsningsforslag innleveres i gruppe-repositorien.
 GRUPPENR: 13
-STUDENTER: Morten Amundsen, Nora Krogh, Erlend Sætre, Marius Fossli, Joakim Kilen, Tina Kvist
+STUDENTER: Morten Amundsen, Nora Krogh, Erlend Sætre, Marius Fossli, Joakim Kilen
 '''
 
 def code():
-    '''
+    '''	
     Implements a code: ASCII mapping from binary string to ASCII string
     and the other way around. Load the tables in memory. 
     '''
-    ascii = {'01000001':'A', ...}
-    binary = {'A':'01000001', ...}
-    return (ascii, binary) # A suggested way to load tables in memory
+    
+    binary = {}
+    ascii = {}
+    
+    # Generate ascii code
+    for i in range(0,128) :
+        ascii[format(i,'08b')] = chr(i)
+    
+    
+    # Reverse the ascii code, this will be binary
+    for k, v in ascii.iteritems():
+        binary[v] = binary.get(v, [])
+        binary[v].append(k)   
+    
+    return ascii # A suggested way to load tables in memory
 
 def encode():
     pass
 
-def decode(sourcecode,n):
+def decode(sourcecode,n, ascii):
     '''
     Decode a sourcecode using chunks of size n
     '''
     # You will need a dictionary holding the mapping between binary string and ASCII chars
+    
+    sentence = ""    
     
     f = open(sourcecode, mode='rb') # Open a file with filename <sourcecode>
     while True:
@@ -31,9 +45,13 @@ def decode(sourcecode,n):
         if chunk == '':             # This is one way to check for the End Of File in Python 
             break
         if chunk != '\n':
-            # Your code goes here, process is a placeholder, process chunk
-            # Return the decoded message
-            pass
+            
+            setence = ""            # The ascii sentence generated
+            
+            # create a s
+            sentence = sentence + ascii[chunk]
+            
+    return sentence
 
 def test():
     '''
@@ -42,4 +60,7 @@ def test():
     but for a temporary testing in a development version can be done 
     directly in the module.
     '''
-    pass
+    
+    print decode('c:/users/morten/desktop/IS-105/is-105_2016_gruppe13/uke4/sourcecode/sourcecode.txt', 8, code())
+
+test()
