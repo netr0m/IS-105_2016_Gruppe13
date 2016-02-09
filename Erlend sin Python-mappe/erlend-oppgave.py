302b99ced2d262b5023789b94bf20fff5f63f4c1
mortea15 <mortea15@student.uia.no>
2016-02-09 13:38:07 +0100

    La til erlend sin oppgave i erlend sin branch

---
+++ Erlend sin Python-mappe/erlend-oppgave.py
@@ -0,0 +1,8 @@
+# Erlend
+# This function substract two number and print an output
+a = int(raw_input("Press your first number "))
+b = int(raw_input("Then your second number "))
+
+sum = a - b
+
+print sum
