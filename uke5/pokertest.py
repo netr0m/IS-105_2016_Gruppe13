123b78c603fbd05c4991af7776dabdf6e5cd6ae0
mortea15 <mortea15@student.uia.no>
2016-02-09 09:40:15 +0100

    Hotfix

---
+++ uke5/pokertest.py
@@ -0,0 +1,11 @@
+import unittest
+from poker import poker_hand
+
+class PokerTest(unittest.TestCase):
+    def setUp(self):
+        pass
+    def playPoker(self):
+        self.assertEqual(poker)
+        
+if __name__ == '__main__':
+    unittest.main()
