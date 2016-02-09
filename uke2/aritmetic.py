072cb9ec0ea70ca5d20040f178ff6b110af996af
mortea15 <mortea15@student.uia.no>
2016-02-09 13:33:46 +0100

    Flytt

---
+++ uke2/aritmetic.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+def pluss(x,y):
+    return x + y
+
+def minus(x,y):
+    return x - y
+    
+def gange (x,y):
+    return x * y    
+    
+def dele(x,y):
+    return x / y
+
+def kvadratrot(x,y):
+    return x ** y
+    
+def rest(x,y):
+    return x % y
