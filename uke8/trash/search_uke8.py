2a701e100ceb10a50b12c7b45b415eeca2141245
mortea15 <mortea15@student.uia.no>
2016-02-29 00:09:18 +0100

    Update

--- uke8/trash/search_uke8.py
+++ uke8/trash/search_uke8.py
@@ -9,9 +9,9 @@ name_List.append(["Marius", "Morten", "Nora", "Joakim", "Erlend"])
 charachter_List = []
 charachter_List.append(['U', 'I', 'A', 'K', 'R', 'S', 'N', 'O', 'R'])
 
-def search_fast(name_List, ("Marius")):
+def search_fast(name_List, Marius):
     for item in name_List:
-        if item == "Marius":
+        if item == Marius:
             return True
         return False
     
@@ -19,7 +19,7 @@ def search_fast(name_List, ("Marius")):
 def search_slow(name_List):
     return_value = False
     for item in haystack:
-        if item == "Marius":
+        if item == Marius:
             return_value = True
-            print "Marius"
+            print Marius
         return return_value
