ec064c24baa79ca9c6468cdbe4330a6e98ac07d4
MariusFosseli <mariusfosseli@hotmail.com>
2016-03-03 22:14:20 +0100

    Uke 5 og uke 8
    Mer kode på uke5_1.2.1 - nå med numpy graf + excel pdf
    Excel dokument og pdf for uke8 ICA

--- uke5/oppg_1.2.1.py
+++ uke5/oppg_1.2.1.py
@@ -1,24 +1,34 @@
+#Kilde: http://matplotlib.org/examples/api/barchart_demo.html
 # -*- coding: utf-8 -*-
 # Import setninger for modulene math og pprint
 # Math for regneoppgaver
 import math
 # Pprint for å printe setninger på hver sin linje
 import pprint
+# Numpy og matplotlib for å lage fremvise graf
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Definer verdiene (antall studenter) for hvert fakultet
-HoI = 1829.
 # Definer navn på fakultet
+HoI = 1829.
 HoITekst = "Helse- og idrettsfag"
+
 HoP = 1525.
 HoPTekst = "Humaniora og pedagogikk"
+
 KF = 420.
 KFTekst = "Kunstfag"
+
 ToR = 2166.
 ToRTekst = "Teknologi og realfag"
+
 LU = 1506.
 LUTekst = "Laererutdanningen"
+
 OoS = 3093.
 OoSTekst = "Oekonomi og samfunnsvitenskap"
+
 total = HoI + HoP + KF + ToR + LU + OoS
 
 # Utregning av sannsynlighet for at en student tilhører et gitt fakultet
@@ -33,15 +43,48 @@ OoSPoss = "{:.2f}".format(OoS/total * 100)
 
 # Liste over fakultet og verdier
 UiAStudents = []
-UiAStudents.append([HoITekst, HoI, HoIPoss])
-UiAStudents.append([HoPTekst, HoP, HoPPoss])
-UiAStudents.append([KFTekst, KF, KFPoss])
-UiAStudents.append([ToRTekst, ToR, ToRPoss])
-UiAStudents.append([LUTekst, LU, LUPoss])
-UiAStudents.append([OoSTekst, OoS, OoSPoss])
+UiAStudents.append([HoITekst, HoI, HoIPoss+"%"])
+UiAStudents.append([HoPTekst, HoP, HoPPoss+"%"])
+UiAStudents.append([KFTekst, KF, KFPoss+"%"])
+UiAStudents.append([ToRTekst, ToR, ToRPoss+"%"])
+UiAStudents.append([LUTekst, LU, LUPoss+"%"])
+UiAStudents.append([OoSTekst, OoS, OoSPoss+"%"])
 
 print ("Navn paa fakultet, antall studenter, sannsynlighet")
 # Pprint modul for å printe ut verdiene
 pprint.pprint(UiAStudents)
 
-print ("Som vi ser over, er det lavest sannsynlighet for at \nen gitt student tilhoerer fakultetet for " + KFTekst)
+print ("\nSom vi ser over, er det lavest sannsynlighet for at \nen gitt student tilhoerer fakultetet for " + KFTekst)
+
+
+# Lager lister med tall vi har fått tidligere for å opprette en graf
+students = (HoI, HoP, KF, ToR, LU, OoS)
+probability = (HoIPoss, HoPPoss, KFPoss, ToRPoss, LUPoss, OoSPoss)
+faculties = ('HoI', 'HoP', 'KF', 'ToR', 'LU', 'OoS')
+
+ind = np.arange(len(faculties))  # Plasseringen for grafene
+width = 0.75       # Bredden på grafene
+
+fig, ax = plt.subplots() # Forteller at vi ønsker å bruke en graf
+
+# Henter plassering fra ind, bredde fra width, verdi på barene fra listene og farge
+axStudents = ax.bar(ind, students, width, color='r')
+axProbability = ax.bar(ind, probability, width, color='r')
+
+ax.set_title('Faculty, number of students and probability') # Tittel på grafen
+ax.set_ylabel('Students') # Navn for y aksen
+ax.set_xticks(ind + 0.4) # Plassering på bar-navnene
+ax.set_xticklabels(faculties) # Navnet på barene
+
+# Designer hvor og hvordan verdiene skal vises over grafene
+def autolabel(rects):
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height*100, 
+                '%d' % int(height),
+                ha='center', va='bottom')
+
+# Verdiene ovenfor grafene vises i prosent, og forteller sannsynligheten for at en gitt student tilhører fakultetet
+autolabel(axProbability)
+
+plt.show()
