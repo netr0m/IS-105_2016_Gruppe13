fe8be238d76dccb055663fef6d941953d6706996
mortea15 <mortea15@student.uia.no>
2016-03-15 16:37:32 +0100

    Update og lagt til flere modeller

--- uke10/metoder/changefilecontent.py
+++ uke10/metoder/changefilecontent.py
@@ -1,23 +1,16 @@
-# -*- coding: utf-8 -*-
-# ICA07 / uke 10
-# Gruppe 13: Morten Amundsen, Nora Krogh, Marius Fosseli, Erlend Sætre, Joakim Kilen
-
-# Import the Memory-map module
 import mmap
 
-# Write an example file (creates test1.txt)
-with open("test1.txt", "wb") as f:
+# Write an example file (creates changefile.txt)
+with open("changefile.txt", "wb") as f:
     # Writes the sentence inside ("") to the specified file
     f.write("Karamellen min liker poteter, og du heter Ove Sundberg\n")
 
 # Opens the specified file
-with open("test1.txt", "r+b") as f:
+with open("changefile.txt", "r+b") as f:
         # Memory-map the file, size 0 means the whole file
         mm = mmap.mmap(f.fileno(), 0)
         # Read the content via standard file methods
         print mm.readline()  # Prints the first line of text in the specified file
-        # Read content in the file via slice notation
-        print mm[:10]  # Prints the first mm[:x] bytes, here the first 10 bytes (Karamellen)
         # Update the content in the file using slice notation;
         # Beware that the new content must have the same size as the content being replaced
         # mm[42:] means it replaces the content 42 steps away/bytes from the beginning
