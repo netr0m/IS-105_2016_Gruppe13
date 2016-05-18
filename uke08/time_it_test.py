6147cb4a3dc8997e93b961ecbd261f8fcd3fa318
MariusFosseli <mariusfosseli@hotmail.com>
2016-05-18 19:33:36 +0200

    update etter janus-møte
    La til matplotlib dotted line

--- uke08/time_it_test.py
+++ uke08/time_it_test.py
@@ -1,3 +1,7 @@
+# Numpy og matplotlib for å lage fremvise graf
+import numpy as np
+import matplotlib.pyplot as plt
+# Timeit for for å registrere tid
 from timeit import Timer
 
 def build_list(n):
@@ -18,9 +22,31 @@ timeList = Timer("inx(x,n)",
 timeDict = Timer("inx(x,n)",
                  "from __main__ import n, build_dict, inx; x = build_dict(n)")
 
+value_list = []
+value_dict = []
+
 print("N", "\t", "List", "\t", "Dict")
 for size in range(1000, 100000+1, 5000):
  n = size
  list_secs = timeList.repeat(5,5)
  dict_secs = timeDict.repeat(5,5)
  print(n, "\t", min(list_secs), "\t", min(dict_secs))
+ 
+ value_list.append(min(list_secs))
+ value_dict.append(min(dict_secs))
+ 
+ xList = [1000,6000,11000,16000,21000,26000,31000,36000,41000,46000,51000,56000,61000,66000,71000,76000,81000,86000,91000,96000]
+ yList = value_list
+
+ xDict = [1000,6000,11000,16000,21000,26000,31000,36000,41000,46000,51000,56000,61000,66000,71000,76000,81000,86000,91000,96000]
+ yDict = value_dict
+
+plt.plot(xList, yList, 'ro', label='List time')
+plt.plot(xDict, yDict, 'ro', label='Dictionary time')
+
+plt.axis([0, 100000, 0, max(value_list) + 0.2])
+plt.xlabel('Size')
+plt.ylabel('Time')
+plt.title('Time it test')
+ 
+plt.show()
