97e6183c1912a3dc8a067a4330b04cbfd20ffc03
Morten Kregnes Amundsen <mortea15@student.uia.no>
2016-05-29 21:39:48 +0200

    Update av ICA 01 (Uke 4), fiksing av unittest for Poker

--- uke04/Oppgave 1.2.4/tests_poker.py
+++ uke04/Oppgave 1.2.4/tests_poker.py
@@ -6,6 +6,10 @@ class TestDeal(unittest.TestCase):
         pass
     def test_deal_5_cards(self):
         self.assertEqual(5, 5)
+    
+    def test_deal_5_cards_2_players(self):
+        self.assertEqual(5, 5)
+        self.assertEqual(5, 5)
         
 if __name__ == '__main__':
     unittest.main()
